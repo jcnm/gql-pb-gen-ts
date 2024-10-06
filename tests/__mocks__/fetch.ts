46534c09fdc89274a89d47d40257b0775f1c4075
J. Charles N. M <jcnm@sylorion.com>
2024-10-06 19:43:51 +0200

    Removing tests need to write them from scratch with Mocha or staying with Jest

--- tests/__mocks__/fetch.ts
+++
@@ -1,8 +0,0 @@
-import fetch from 'node-fetch';
-
-jest.mock('node-fetch', () => ({
-  __esModule: true,
-  default: jest.fn(() => Promise.resolve({
-    json: () => Promise.resolve({ data: 'mocked data' })
-  }))
-}));
